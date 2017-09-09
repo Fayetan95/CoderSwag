ed081989de751f2fc53431afe34b490b06e402c9
Fayetan95 <fayetan95@gmail.com>
2017-09-09 18:20:39 +0800

    lesson 03

---
+++ CoderSwag/CoderSwag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Tan Juat Fwee on 9/9/17.
+//  Copyright © 2017 Faye Tan. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
